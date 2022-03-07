eddafd39fab52b6a4416021bdf9058b31358e63f
Alfie9087 <antonio.castillo@obf.ateneo.edu>
2022-03-08 01:59:16 +0800

    Fixed admin.py
    note: create super user
    
    fixed admin page

--- enlistmentbuddy/enlistmentbuddyapp/admin.py
+++ enlistmentbuddy/enlistmentbuddyapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import IndexCard
+
+class IndexCardAdmin(admin.ModelAdmin):
+    model = IndexCard
+
+class IndexCardInline(admin.TabularInline):
+    model = IndexCard
+    
+
+admin.site.register(IndexCard, IndexCardAdmin)
+
+
 # Register your models here.
