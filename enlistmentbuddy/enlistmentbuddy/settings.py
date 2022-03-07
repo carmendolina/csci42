afed95b355963947a9e35c35ee15c3956c5c8c94
Carmen Dolina <carmencita.dolina@obf.ateneo.edu>
2022-03-07 15:42:45 +0800

    testing settings

--- enlistmentbuddy/enlistmentbuddy/settings.py
+++ enlistmentbuddy/enlistmentbuddy/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'enlistmentbuddy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'csci42',
+        'USER': 'postgres',
+        'USER': 'carmen',
+        'PASSWORD': 'user password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
