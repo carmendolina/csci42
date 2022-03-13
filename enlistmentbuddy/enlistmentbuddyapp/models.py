bfad2de0d3a1e50bdd455c3e6cdc81fbb3708c14
RaffLLE <rafael.evangelista@obf.ateneo.edu>
2022-03-13 17:03:48 +0800

    models.py adjusted - __str__ added

--- enlistmentbuddy/enlistmentbuddyapp/models.py
+++ enlistmentbuddy/enlistmentbuddyapp/models.py
@@ -14,3 +14,6 @@ class IndexCard(models.Model):
     end = models.TimeField(auto_now=False, auto_now_add=False, null=True)
     venue = models.CharField(max_length=20, null=True)
     professor = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return '{} {} {}-{}'.format(self.code, self.section, self.start, self.end)
