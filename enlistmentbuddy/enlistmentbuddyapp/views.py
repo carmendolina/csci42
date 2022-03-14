fc317d72a2b684a11fe7b1ee7333446fffb4b529
Mewwee <ana.fuentes@obf.ateneo.edu>
2022-03-14 18:13:28 +0800

    somewhat functional days of the week
    c:

--- enlistmentbuddy/enlistmentbuddyapp/views.py
+++ enlistmentbuddy/enlistmentbuddyapp/views.py
@@ -22,26 +22,32 @@ def index(request):
 def index_card_view(request):
     form = IndexCardForm(request.POST)
     classes = IndexCard.objects.all()
-    thislist = setting_time();
+    thislist = setting_time()
+    mondaylist = monday()
+    wednesdaylist = wednesday()
+
     if request.method == 'POST':
     # Checking if the inputs are valid
         if form.is_valid():
             form.save()
-            #return render(
-                #'Code: {} Section: {} Schedule: {} Start Time: {} End Time: {} Venue: {} Professor: {}'.format(
-                # Getting data from our form that have been validated
-                #form.cleaned_data['code'],
-                #form.cleaned_data['section'],
-                #form.cleaned_data['sched'],
-                #form.cleaned_data['start'],
-                #form.cleaned_data['end'],
-                #form.cleaned_data['venue'],
-                #form.cleaned_data['professor']
-                #)
             return redirect("index_card")
     else:
         form = IndexCardForm()
-    return render(request, 'index.html', {'form': form, 'class_info': classes, 'range': range(0,5+1), 'time': thislist})
+
+    return render(request, 'index.html', {'form': form, 'class_info': classes, 'range': range(0,5+1), 'time': thislist, 'monday': mondaylist, 'wednesday': wednesdaylist})
+
+
+def monday():
+    mondays = []
+    for mondayclass in IndexCard.objects.filter(sched__contains='M'):
+        mondays.append(mondayclass)
+    return mondays
+
+def wednesday():
+    wednesdays = []
+    for wednesdayclass in IndexCard.objects.filter(sched__contains='W'):
+        wednesdays.append(wednesdayclass)
+    return wednesdays
 
 def setting_time():
     starttime = datetime.datetime(100,1,1,7,00,00) # first 3 are dummy last 3 are hour/min/sec
