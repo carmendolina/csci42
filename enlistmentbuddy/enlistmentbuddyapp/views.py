16010e9a85c49813afdb803505a9ab6c09174711
RaffLLE <rafael.evangelista@obf.ateneo.edu>
2022-03-14 16:58:14 +0800

    views.py setting_time function added - sets start, end and interval index.html filters proper timeslots

--- enlistmentbuddy/enlistmentbuddyapp/views.py
+++ enlistmentbuddy/enlistmentbuddyapp/views.py
@@ -12,6 +12,8 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import DeleteView
 from django.views.generic.detail import DetailView
 
+import datetime
+
 #insert functions here
 def index(request):
     return redirect('index_card')
@@ -20,6 +22,7 @@ def index(request):
 def index_card_view(request):
     form = IndexCardForm(request.POST)
     classes = IndexCard.objects.all()
+    thislist = setting_time();
     if request.method == 'POST':
     # Checking if the inputs are valid
         if form.is_valid():
@@ -38,4 +41,16 @@ def index_card_view(request):
             return redirect("index_card")
     else:
         form = IndexCardForm()
-    return render(request, 'index.html', {'form': form, 'class_info': classes})
+    return render(request, 'index.html', {'form': form, 'class_info': classes, 'range': range(0,5+1), 'time': thislist})
+
+def setting_time():
+    starttime = datetime.datetime(100,1,1,7,00,00) # first 3 are dummy last 3 are hour/min/sec
+    endtime = datetime.datetime(100,1,1,21,30,00)
+    timelist = [];
+    while (starttime < endtime):
+        timelist.append(starttime.time);
+        starttime += datetime.timedelta(0,30*60) # days, seconds, then other fields.
+    return timelist;
+
+#sources:
+#Time - https://stackoverflow.com/questions/100210/what-is-the-standard-way-to-add-n-seconds-to-datetime-time-in-python
