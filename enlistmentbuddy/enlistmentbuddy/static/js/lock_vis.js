a85cd6d9deae9a84ca0f88f2b203d245f223d9c1
Carmen Dolina <carmencita.dolina@obf.ateneo.edu>
2022-04-19 17:31:26 +0800

    locks visually

--- enlistmentbuddy/enlistmentbuddy/static/js/lock_vis.js
+++ enlistmentbuddy/enlistmentbuddy/static/js/lock_vis.js
@@ -1,10 +1,17 @@
 const ulcourses = document.getElementsByClassName('day unlocked');
 const lcourses = document.getElementsByClassName('day locked');
 
+function getButton(div) {
+    var buttonimg = div.firstElementChild.firstElementChild.firstElementChild.firstElementChild;
+    return buttonimg;
+}
+
 function lock(course) {
     if (course.className == 'day unlocked') {
         course.className = "day locked";
+        getButton(course).src = "../static/img/button-lock.svg";
     } else if (course.className == 'day locked') {
         course.className = "day unlocked";
+        getButton(course).src = "../static/img/button-unlock.svg";
     }
 }
