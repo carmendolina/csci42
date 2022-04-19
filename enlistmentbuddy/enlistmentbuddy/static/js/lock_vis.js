4fb7704f901c7a056df607b31c2d14639856e472
Carmen Dolina <carmencita.dolina@obf.ateneo.edu>
2022-04-19 15:16:01 +0800

    changes class from unlocked to locked

--- enlistmentbuddy/enlistmentbuddy/static/js/lock_vis.js
+++ enlistmentbuddy/enlistmentbuddy/static/js/lock_vis.js
@@ -1,3 +1,10 @@
-function lock() {
-    
+const ulcourses = document.getElementsByClassName('day unlocked');
+const lcourses = document.getElementsByClassName('day locked');
+
+function lock(course) {
+    if (course.className == 'day unlocked') {
+        course.className = "day locked";
+    } else if (course.className == 'day locked') {
+        course.className = "day unlocked";
+    }
 }
