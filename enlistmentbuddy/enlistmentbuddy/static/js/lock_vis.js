245df179a42c4a2e0933b36d0b56fa763bb8b3c0
Carmen Dolina <carmencita.dolina@obf.ateneo.edu>
2022-04-30 15:42:41 +0800

    generate button returns and prints locked list

--- enlistmentbuddy/enlistmentbuddy/static/js/lock_vis.js
+++ enlistmentbuddy/enlistmentbuddy/static/js/lock_vis.js
@@ -1,3 +1,5 @@
+var lockedClasses = [];
+
 function getButton(div) {
     var buttonimg = div.firstElementChild.firstElementChild.firstElementChild;
     return buttonimg;
@@ -8,11 +10,21 @@ function lock(course) {
         course.classList.remove("unlocked");
         course.classList.add("locked");
         getButton(course).src = "../static/img/button-lock.svg";
+        if (!lockedClasses.includes(course.classList)) {
+            lockedClasses.push(course.classList);
+        }
     } else if (course.classList.contains("locked")) {
-        
         course.classList.remove("locked");
         course.classList.add("unlocked");
         getButton(course).src = "../static/img/button-unlock.svg";
+        if (lockedClasses.includes(course.classList)) {
+            lockedClasses.pop(course.classList);
+        }
     }
-    console.log(course.classList)
+    console.log(lockedClasses);
+}
+
+function returnLockedList() {
+    console.log(lockedClasses);
+    return lockedClasses;
 }
