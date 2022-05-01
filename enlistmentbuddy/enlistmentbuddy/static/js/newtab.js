9350b5770f13961999ceceaff60387e160d25c0e
Carmen Dolina <carmencita.dolina@obf.ateneo.edu>
2022-05-02 00:43:41 +0800

    sched splits normally now

--- enlistmentbuddy/enlistmentbuddy/static/js/newtab.js
+++ enlistmentbuddy/enlistmentbuddy/static/js/newtab.js
@@ -1,10 +1,10 @@
 var schedulelist = []
 var templist = []
 
-function pain(schedule){
-    console.log(schedule);
-    templist = schedule;
-    for (let i = 0; i < templist.length; i++) {
-        console.log(templist[i]);
-    } 
+function pain(sched){
+    templist = (Array.from(sched)).join('');
+    templist = templist.slice(1);
+    templist = templist.slice(0, templist.length - 1);
+    templist = templist.split(", ")
+    console.log(templist);
 }
