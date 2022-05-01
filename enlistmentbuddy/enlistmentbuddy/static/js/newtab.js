7199e4e52f2f0ad39b820d3bfca25ae0d1304721
RaffLLE <rafael.evangelista@obf.ateneo.edu>
2022-05-01 15:51:36 +0800

    newtab.js added index.html updated to include newtab

---
+++ enlistmentbuddy/enlistmentbuddy/static/js/newtab.js
@@ -0,0 +1,10 @@
+var schedulelist = []
+var templist = []
+
+function pain(schedule){
+    console.log(schedule);
+    templist = schedule;
+    for (let i = 0; i < templist.length; i++) {
+        console.log(templist[i]);
+    } 
+}
