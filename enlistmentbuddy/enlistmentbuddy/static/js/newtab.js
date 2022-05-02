86cf7113d04f7e0be6229b6e921f0f0e8e7d6492
Carmen Dolina <carmencita.dolina@obf.ateneo.edu>
2022-05-02 20:53:24 +0800

    persistent tab ui

--- enlistmentbuddy/enlistmentbuddy/static/js/newtab.js
+++ enlistmentbuddy/enlistmentbuddy/static/js/newtab.js
@@ -1,5 +1,7 @@
 
 const tabs = document.querySelectorAll(".scheduleGrid");
+const tablinks = document.querySelectorAll(".tablinks");
+tablinks[0].style.backgroundColor = "var(--midpurple2)";
 tabs[1].style.display = "none";
 
 function returnSched(sched) {
@@ -15,9 +17,13 @@ function returnSched(sched) {
 function displayGenerator() {
     tabs[0].style.display = "grid";
     tabs[1].style.display = "none";
+    tablinks[0].style.backgroundColor = "var(--midpurple2)";
+    tablinks[1].style.backgroundColor = "var(--lightpurple)";
 }
 
 function displayTab1() {
     tabs[0].style.display = "none";
     tabs[1].style.display = "grid";
+    tablinks[1].style.backgroundColor = "var(--midpurple2)";
+    tablinks[0].style.backgroundColor = "var(--lightpurple)";
 }
