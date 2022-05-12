4aa9c55bd57d343883c9983cdb89b9ed89b8ecd8
Mewwee <ana.fuentes@obf.ateneo.edu>
2022-05-12 13:46:16 +0800

    Functional delete tabs on generate

--- enlistmentbuddy/enlistmentbuddy/static/js/newtab.js
+++ enlistmentbuddy/enlistmentbuddy/static/js/newtab.js
@@ -3,6 +3,8 @@ const tabs = document.querySelectorAll(".scheduleGrid");
 const tablinks = document.querySelectorAll(".tablinks");
 const tabheaders = document.querySelectorAll(".tabhead");
 tablinks[0].style.backgroundColor = "var(--midpurple2)";
+
+tabs[1].style.display = "none";
 for (let i = 1; i < tabs.length; i++) {
     tabs[i].style.display = "none";
     tablinks[i].style.backgroundColor = "var(--lightpurple)";
@@ -52,5 +54,5 @@ function deleteTab(tabnum) {
 // hi ana this is what i need passed into views <3
 // its var deletedtabs tysm
 function returnDeletedTabs() {
-
+    document.getElementById("returndeltab").value = deletedtabs;
 }
