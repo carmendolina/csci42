a1589486002d196251df1eb3634b716e1cd15514
Carmen Dolina <carmencita.dolina@obf.ateneo.edu>
2022-05-15 14:56:59 +0800

    all frontend pin works if ajax no reload

--- enlistmentbuddy/enlistmentbuddy/static/js/newtab.js
+++ enlistmentbuddy/enlistmentbuddy/static/js/newtab.js
@@ -1,35 +1,52 @@
 
-var tabs = document.querySelectorAll(".scheduleGrid");
-var tablinks = document.querySelectorAll(".tablinks");
+var tabs = document.querySelectorAll(".scheduleGrid"); // length = 1
+var tablinks = document.querySelectorAll(".tablinks"); // length = 1
 const tabheaders = document.querySelectorAll(".tabhead");
-const timetable = document.getElementById("timetable");
+var timetable = document.getElementById("timetable");
 const timetableHead = document.querySelectorAll(".timetableHead");
 var pinCounter = 0;
 var pins = [];
+var pinscheds = [];
 tablinks[0].style.backgroundColor = "var(--midpurple2)";
 
-tabs[1].style.display = "none";
-for (let i = 1; i < tabs.length; i++) {
-    tabs[i].style.display = "none";
-    tablinks[i].style.backgroundColor = "var(--lightpurple)";
-    console.log(tabs[i]);
-}
-var deletedtabs = [];
+// tabs[1].style.display = "none";
+// for (let i = 1; i < tabs.length; i++) {
+//     tabs[i].style.display = "none";
+//     tablinks[i].style.backgroundColor = "var(--lightpurple)";
+//     console.log(tabs[i]);
+// }
+// var deletedtabs = [];
 
 function returnStaticHome() {
     pinCounter = pinCounter + 1;
-    // tabs.push(tabs[0].cloneNode(true));
-    timetable.appendChild(tabs[0].cloneNode(true)).style.display = "none";
-    var newtab = document.createElement("div")
+    var newsched = tabs[0].cloneNode(true);
+    pinscheds.push(newsched);
+    
+    var newtab = document.createElement("div");
     newtab.className = "tablinks";
-    var newbutton = document.createElement("button");
-    newbutton.innerHTML = pinCounter;
-    newtab.setAttribute( "onClick", "displayTab1(pinCounter-1);" );
-    newtab.appendChild(newbutton);
-    timetableHead[0].appendChild(newtab);
+    pins.push(newtab);
+    newtab.setAttribute( "onClick", "displayTab1(pins.indexOf(this)+1);" );
+
+    timetable.appendChild(newsched).style.display = "none";
     tabs = document.querySelectorAll(".scheduleGrid");
     tablinks = document.querySelectorAll(".tablinks");
-    console.log(tablinks);
+    
+    var newclose = document.createElement("img");
+    newclose.setAttribute("src", "../static/img/button-add.svg");
+    newclose.setAttribute("class", "closetab");
+    newclose.setAttribute("onClick", "deleteTab(this.parentElement)");
+    
+    var newbutton = document.createElement("button");
+    newbutton.innerHTML = pins.indexOf(newtab)+1;
+    
+    newtab.appendChild(newclose);
+    newtab.appendChild(newbutton);
+    timetableHead[0].appendChild(newtab);
+
+    console.log(pins);
+
+    tabs = document.querySelectorAll(".scheduleGrid");
+    tablinks = document.querySelectorAll(".tablinks");
 }
 
 function returnSched(sched) {
@@ -46,37 +63,59 @@ function returnSched(sched) {
 function displayGenerator() {
     tabs = document.querySelectorAll(".scheduleGrid");
     tablinks = document.querySelectorAll(".tablinks");
+    tabs[0].style.display = "grid";
+    tablinks[0].style.backgroundColor = "var(--midpurple2)";
+    tablinks[0].addEventListener("mouseout", function() {
+        tablinks[0].style.backgroundColor = "var(--midpurple2)";
+    });
     for (let i = 1; i < tabs.length; i++) {
         tabs[i].style.display = "none";
         tablinks[i].style.backgroundColor = "var(--lightpurple)";
     }
-    tabs[0].style.display = "grid";
-    tablinks[0].style.backgroundColor = "var(--midpurple2)";
 }
 
 function displayTab1(tabnum) {
     tabs = document.querySelectorAll(".scheduleGrid");
     tablinks = document.querySelectorAll(".tablinks");
-    console.log(tablinks[0]);
     tablinks[0].style.backgroundColor = "var(--lightpurple)";
     for (let i = 0; i < tabs.length; i++) {
         tabs[i].style.display = "none";
         tablinks[i].style.backgroundColor = "var(--lightpurple)";
-        console.log(tabs[i]);
+        tablinks[i].addEventListener("mouseover", function() {
+            tablinks[i].style.backgroundColor = "var(--midpurple2)";
+        });
+        tablinks[i].addEventListener("mouseout", function() {
+            tablinks[i].style.backgroundColor = "var(--lightpurple)";
+        });
     }
     tabs[tabnum].style.display = "grid";
-    tablinks[tabnum+1].style.backgroundColor = "var(--midpurple2)";
+    tablinks[tabnum].style.backgroundColor = "var(--midpurple2)";
+    tablinks[tabnum].addEventListener("mouseout", function() {
+        tablinks[tabnum].style.backgroundColor = "var(--midpurple2)";
+    });
 }
 
-function deleteTab(tabnum) {
-    console.log(tabnum);
-    tabheaders[tabnum-1].style.display = "none";
-    deletedtabs.push(tabnum-1);
-    console.log(deletedtabs);
+function deleteTab(div) {
+    // console.log(tabnum);
+    // tabheaders[tabnum-1].style.display = "none";
+    // deletedtabs.push(tabnum-1);
+    // console.log(deletedtabs);
+    displayGenerator()
+    pins.splice(pins.indexOf(div),1);
+    pinCounter = pinCounter - 1;
+    console.log(pins);
+    for (let i = 0; i < pins.length; i++) {
+        pins[i].children[1].innerHTML = i+1;
+    }
+    // timetable.removeChild(tabs[indexOf(div)+1]);
+    timetableHead[0].removeChild(div);
+
+    // tabs = document.querySelectorAll(".scheduleGrid");
+    // tablinks = document.querySelectorAll(".tablinks");
 }
 
 // hi ana this is what i need passed into views <3
 // its var deletedtabs tysm
-function returnDeletedTabs() {
-    document.getElementById("returndeltab").value = deletedtabs;
-}
+// function returnDeletedTabs() {
+//     document.getElementById("returndeltab").value = deletedtabs;
+// }
